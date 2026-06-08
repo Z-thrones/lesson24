1c146ccaaa3095fcd607e0f64eb07a5bd71c4db2
Z-thrones <zaviyar.ktk@gmail.com>
2026-06-08 18:29:42 +0500

    Add files via upload

---
+++ A64.py
@@ -0,0 +1,13 @@
+import math
+ceil = math.ceil(23.56)
+floor = math.floor(23.56)
+print("The result for ceil the number 23.56 is " , ceil , " and the result for floor of 23.56 is " , floor)
+x = 10
+y = -15
+copysign = math.copysign(x , y)
+print("The reslut ofcopysigning 10 and -15 is " , copysign)
+fabs1 = math.fabs(-96)
+fabs2 = math.fabs(56)
+print("The reslut of Fabs -96 is " , fabs1 , " and the reslut of fabs 56 is " , fabs2)
+gcd = math.gcd(24 , 56)
+print("The gcd of 24 and 56 is " , gcd)
